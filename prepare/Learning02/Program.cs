dd036fc59d9499c64409cf06dd79b149e7c36383
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-09-18 21:21:59 -0300

    Publish Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+using Learning02;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+ 
+
+        Job job0 = new Job();
+        job0._jobTitle="Software Engineer" ;
+        job0._company="Microsoft";
+        job0._startYear = 2019;
+        job0._endYear = 2022;
+
+        Job job1 = new Job();
+        job1._jobTitle = "Manager";
+        job1._company = "Apple";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+
+      //  job0.Display();
+
+        Resume myresume = new Resume();
+        myresume._name = "Allison Rose";
+        myresume.add(job0);
+        myresume.add(job1);
+
+        myresume.Display();
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Learning02
+{
+
+
+    public class Resume
+      {
+
+        public Resume()
+        {
+            _name = "";
+        }
+        public string _name;
+        public List<Job> jobs = new List<Job>();
+
+        public void add( Job props)
+        {
+            jobs.Add(props);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name:{_name}");
+            Console.WriteLine("Jobs:");
+            foreach ( Job unit in jobs)
+            {
+                unit.Display();
+            }
+
+        }//--------------
+    }//-- Resume
+}//----------Learning02
