79cbd87864122b47411bbeb48ea9bc685d132a8f
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-09-19 22:11:15 -0300

    update

---
+++ team/W03/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace W03
+{
+    public class FileManager
+    {
+        public string _filename { get; set; }
+        public int MyProperty { get; set; }
+
+        v
+        public bool isEmptyFileName()
+        {
+            return (_filename.Length <= 2);
+        }
+
+        public FileManager()
+        {
+            _filename = "";
+        }
+
+        public read_file()
+        {
+
+        }
+
+    }
+}
---
+++ team/W03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace W03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            sss
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
