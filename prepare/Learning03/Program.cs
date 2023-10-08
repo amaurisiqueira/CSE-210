a88976bd4f43f83b7dd89ddc6ff15bc75494b7e1
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-10-08 19:57:49 -0300

    Exercise of encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,30 @@
-using System;
+﻿using System;
 
-class Program
+namespace Learning03
 {
-    static void Main(string[] args)
+    class MainClass
     {
-        Console.WriteLine("Hello Learning03 World!");
+        public static void Main(string[] args)
+        {
+
+            Fraction p1 = new Fraction();
+            Console.WriteLine(p1.GetFractionString());
+            Console.WriteLine(p1.GetDecimalValue());
+
+            Fraction p2 = new Fraction(5,1);
+            Console.WriteLine(p2.GetFractionString());
+            Console.WriteLine(p2.GetDecimalValue());
+
+            Fraction p3 = new Fraction(3, 4);
+            Console.WriteLine(p3.GetFractionString());
+            Console.WriteLine(p3.GetDecimalValue());
+
+            Fraction p4 = new Fraction(1, 3);
+            Console.WriteLine(p4.GetFractionString());
+            Console.WriteLine(p4.GetDecimalValue());
+
+
+ 
+        }
     }
 }
