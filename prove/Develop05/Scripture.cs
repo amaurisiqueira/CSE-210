f68640fa35e5cc308eca28dac82d743139f7815f
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-10-09 21:43:09 -0300

    week 5 prove

---
+++ prove/Develop05/Scripture.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Develop05
+{
+    public class Scripture
+    {
+        private Reference _reference;
+        private int _n_hiden;
+        private int _n_word_total;
+        List<Word> _words;
+
+        public Scripture(string book, int chapter, int verse_start, string content)
+        {
+            _n_hiden = 0;
+            _n_word_total = 0;
+            _reference = new Reference(book, chapter, verse_start, verse_start);
+            _words = new List<Word>();
+
+            string[] array = content.Split(' ');
+            foreach (string x in array)
+            {
+                Word myNew = new Word(x);
+                _words.Add(myNew);
+                _n_word_total++;
+
+            }
+
+        }//-------------------------
+
+        public Scripture(string book, int chapter, int verse_start, int verse_end, string content)
+        {
+            _n_hiden = 0;
+            _n_word_total = 0;
+            _reference = new Reference(book, chapter, verse_start, verse_end);
+            _words = new List<Word>();
+
+            string[] array = content.Split(' ');
+            foreach (string x in array)
+            {
+                Word myNew = new Word(x);
+                _words.Add(myNew);
+                _n_word_total++;
+
+            }
+
+        }//-------------------
+
+        public int GetQuantityWord()
+        {
+
+            return _n_word_total;
+        }
+
+        public int GetQuantityHiden()
+        {
+
+            return _n_hiden;
+        }
+        public bool IsWorking()
+        {
+            //    Console.WriteLine($"{GetQuantityWord()} > 0  && {GetQuantityHiden()} <  {GetQuantityWord()}");
+
+            if (GetQuantityWord() > 0 && GetQuantityHiden() < GetQuantityWord()) return true;
+
+            return false;
+        }
+
+
+        public void PrintTexts()
+        {
+            Console.Write(_reference.GetText() + " ");
+            foreach (Word x in _words)
+            {
+                Console.Write(x.GetWord() + " ");
+
+            }
+        }//------------------
+
+        public void HideRandomly()
+        {
+
+            List<int> candidate = new List<int>();
+            for (int x = 0; x < _words.Count; x++)
+            {
+                if (_words[x].GetVisible())
+                {
+                    candidate.Add(x);
+                }
+            }//end for
+
+            if (candidate.Count > 0)
+            {
+
+                Random rnd = new Random();
+
+                //Console.WriteLine(candidate.Count);
+
+
+                int tmp_candidate = rnd.Next(1, candidate.Count) - 1;
+                //Console.WriteLine(candidate[tmp_candidate]);
+
+                _words[candidate[tmp_candidate]].SetUnVisible();
+                _n_hiden++;
+
+            }
+
+        }//-----------end of HideRandomly
+
+    }
+}
---
+++ prove/Develop05/Word.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Develop05
+{
+    public class Word
+    {
+        private string  _word;
+        private bool    _visible;
+        public Word(string word)
+        {
+            _word = word;
+            _visible = true;
+        }
+
+        public bool GetVisible()
+        {
+            return _visible;
+        }
+
+        public void SetUnVisible()
+        {
+            _visible = false;
+        }
+
+        public string GetWord()
+        {
+            if (!_visible)
+            {
+
+                string _myreturn = "";
+                for(int x=0;x< _word.Length; x++)
+                {
+                    _myreturn += "_";
+                }
+                return _myreturn;
+            }
+
+            return _word;
+        }
+
+    }
+}
