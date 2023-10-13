8ae09b6f34f3cd44f7c96229d74169fcdaceeec5
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-10-12 21:00:58 -0300

    Update folder work 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,115 @@
-using System;
+﻿using System;
 
-class Program
+namespace Develop04
 {
-    static void Main(string[] args)
+    class MainClass
     {
-        Console.WriteLine("Hello Develop02 World!");
+      static   System.Collections.Generic.List<string> _myMenu = new System.Collections.Generic.List<string>
+            {
+                "Write",
+                "Display",
+                "Load",
+                "Save",
+                "Quit"
+            };
+
+        static Journal _jornal = new Journal() ;
+        private const int MAXMENU = 5;
+        private const string WELCOME = "Welcome to the Journal Program!";
+
+        static private void ListMainMenu()
+        {
+            Console.WriteLine(WELCOME);
+            Console.WriteLine("");
+            int n = 0;
+            foreach (string mymenu in _myMenu)
+            {
+                Console.WriteLine($"{++n}) {mymenu}");
+
+            }
+        }//-------------------------
+
+        static private int choice()
+        {
+            while (true)
+            {
+                Console.Write("What would you like to do?");
+                string tmp = "";
+                try
+                {
+                    tmp = Console.ReadLine().Trim();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Invalid format !!!{e.ToString() }");
+                }
+
+
+                tmp = tmp.Replace('\0', ' ');
+                tmp = tmp.Trim();
+                if (tmp.Length < 1 || tmp == "\0") tmp = "0";
+                int myoption = Convert.ToInt16(tmp);
+              
+             
+                if (myoption<1 || myoption > _myMenu.Count)
+                {
+                    Console.WriteLine("Please, type a valid number!!!!!!");
+                    continue;
+                }
+
+                return myoption-1;
+
+            }//-------------------------------------------
+
+
+        }//---- end of choice
+
+        public static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+
+
+             bool live = true;
+             while (live)
+            {
+                ListMainMenu();
+                switch (choice()) {
+                    case 0:
+                        {
+                            _jornal.Write();
+                            break;
+                        }
+                    case 1:
+                        {
+                            _jornal.Display();
+                            break;
+                        }
+                    case 2:
+                        {
+                            _jornal.Load();
+                            break;
+                        }
+                    case 3:
+                        {
+                            _jornal.Save();
+                            break;
+                        }
+                    case 4:
+                        {
+                            live = false;
+                            break;
+                        }
+
+                }
+
+
+            }//end while
+
+            Console.WriteLine("\n\nBYE !!!!!!");
+
+        }
     }
 }
--- team/W03/typePrompt.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace W03
-{
-    public struct typePrompt
-    {
-        public string id { set; get; }
-        public string description { set; get; }
-    }
-}
