1c6ddadb29568cac894613f8db3d83ba4502af30
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-10-22 17:23:56 -0300

    add exceed module

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Develop03
 {
     public struct tmp_data
     {
-        private string _book;
-        private int _chapter;
-        private int _verse_start;
-        private int _verse_end;
-        private string _content;
+        public string _book;
+        public int _chapter;
+        public int _verse_start;
+        public int _verse_end;
+        public string _content;
         public tmp_data(string book, int chapter, int verse_start, int verse_end, string content)
         {
             _book = book;
@@ -27,16 +28,19 @@ namespace Develop03
 
     class Develop03
     {
+
         public static void Main(string[] args)
         {
             Random rnd = new();
             int dificult_level = 3;
-            List<tmp_data> myCollection = new()
+            List<tmp_data> myCollection = LoadFile("scripts.json");
+
+            if (myCollection.Count==0)
             {
-               new( "Proverbs", 3, 5, 6, "Trust in the Lord with all your heart and do not rely upon your understanding. Know Him in all your ways, and He will direct your paths.") ,
-               new( "Psalms",1,1,1,"The praises of a man are that he did not follow the counsel of the wicked, neither did he stand in the way of sinners nor sit in the company of scorners."),
-               new( "Psalms",23,1,2,"A song of David. The Lord is my shepherd; I shall not want. He causes me to lie down in green pastures; He leads me beside still waters."),
-            };
+                Console.WriteLine("No have scripts to show!");
+                return;
+            }
+
             tmp_data myOption = myCollection[rnd.Next(1, myCollection.Count) - 1];
             // Console.WriteLine(myOption.book);
             Scripture myscript;
@@ -60,6 +64,22 @@ namespace Develop03
                 if (!myscript.IsWorking()) break;
                 for (int dificult = 0; dificult < dificult_level; dificult++) myscript.HideRandomly();
             }
+        }
+
+
+        private static List<tmp_data> LoadFile(string myFile)
+        {
+            List<tmp_data> data = new List<tmp_data>();
+            if (File.Exists(myFile))
+            {
+                using (StreamReader r = new StreamReader(myFile))
+                {
+                    string json = r.ReadToEnd();
+                    Console.WriteLine(json);
+                    data = JsonConvert.DeserializeObject<List<tmp_data>>(json);
+                }
+            }
+            return data;
         }
     }
 }
