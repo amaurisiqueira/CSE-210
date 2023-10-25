eebd44ffeb14003e61812d1a7a704d3873d7cd8a
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-10-24 22:26:14 -0300

    learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //simple assignment
+        Assignment assignment = new("Samuel Bennet", "Multiplication");
+        assignment.GetSummary();
+        // mathAssignment
+        MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        mathAssign.GetHomeworksList();
+        // writing 
+        WriteAssignment writeAssign = new("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        writeAssign.GetWritingInformation();
+
     }
 }
